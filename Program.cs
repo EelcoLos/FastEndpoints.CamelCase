2958a53012e4b9e10666469fdea337a25c063dbf
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2024-03-08 16:23:28 +0100

    formatting

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ var bld = WebApplication.CreateBuilder();
 bld.Services
    .AddFastEndpoints()
    .SwaggerDocument(p =>
-    p.DocumentSettings = s =>
-    {
-        s.SerializerSettings.ContractResolver = new DefaultContractResolver
-        {
-            NamingStrategy = new CamelCaseNamingStrategy()
-        };
-    }); //define a swagger document
+	p.DocumentSettings = s =>
+	{
+		s.SerializerSettings.ContractResolver = new DefaultContractResolver
+		{
+			NamingStrategy = new CamelCaseNamingStrategy()
+		};
+	}); //define a swagger document
 
 var app = bld.Build();
 app.UseFastEndpoints()
@@ -21,107 +21,107 @@ app.Run();
 
 public class PostUser : Endpoint<MyRequest, MyResponse>
 {
-   public override void Configure()
-   {
-      Post("/api/user/create");
-      AllowAnonymous();
-   }
+	public override void Configure()
+	{
+		Post("/api/user/create");
+		AllowAnonymous();
+	}
 
-   public override async Task HandleAsync(MyRequest req, CancellationToken ct)
-   {
-      var id = Guid.NewGuid();
-      var succeeded = GlobalDataStore.DataStore.TryAdd(id, req);
-      if (!succeeded)
-      {
-         await SendErrorsAsync(500, ct);
-         return;
-      }
-      var response = MapToResponse(req, id); // Convert entity to MyResponse
-      await SendCreatedAtAsync<GetUser>("GetUser", response, cancellation: ct); // Pass response instead of entity
-   }
+	public override async Task HandleAsync(MyRequest req, CancellationToken ct)
+	{
+		var id = Guid.NewGuid();
+		var succeeded = GlobalDataStore.DataStore.TryAdd(id, req);
+		if (!succeeded)
+		{
+			await SendErrorsAsync(500, ct);
+			return;
+		}
+		var response = MapToResponse(req, id); // Convert entity to MyResponse
+		await SendCreatedAtAsync<GetUser>("GetUser", response, cancellation: ct); // Pass response instead of entity
+	}
 
 
-   private MyResponse MapToResponse(MyRequest entity, Guid id) => // Add this method to convert entity to MyResponse
-      new()
-      {
-         Id = id,
-         FullName = $"{entity.FirstName} {entity.LastName}",
-         IsOver18 = entity.Age >= 18
-      };
+	private MyResponse MapToResponse(MyRequest entity, Guid id) => // Add this method to convert entity to MyResponse
+	   new()
+	   {
+		   Id = id,
+		   FullName = $"{entity.FirstName} {entity.LastName}",
+		   IsOver18 = entity.Age >= 18
+	   };
 }
 
 public class GetUsers : EndpointWithoutRequest<List<MyResponse>>
 {
-   public override void Configure()
-   {
-      Get("/api/users");
-      AllowAnonymous();
-   }
+	public override void Configure()
+	{
+		Get("/api/users");
+		AllowAnonymous();
+	}
 
-   public override async Task HandleAsync(CancellationToken ct)
-   {
-      var response = GlobalDataStore.DataStore.Values.Select(MapToResponse).ToList();
-      await SendOkAsync(response, ct);
-   }
-   private static MyResponse MapToResponse(MyRequest entity, int location) =>
-       new()
-       {
-          Id = GlobalDataStore.DataStore.Keys.ElementAt(location),
-          FullName = $"{entity.FirstName} {entity.LastName}",
-          IsOver18 = entity.Age >= 18
-       };
+	public override async Task HandleAsync(CancellationToken ct)
+	{
+		var response = GlobalDataStore.DataStore.Values.Select(MapToResponse).ToList();
+		await SendOkAsync(response, ct);
+	}
+	private static MyResponse MapToResponse(MyRequest entity, int location) =>
+		new()
+		{
+			Id = GlobalDataStore.DataStore.Keys.ElementAt(location),
+			FullName = $"{entity.FirstName} {entity.LastName}",
+			IsOver18 = entity.Age >= 18
+		};
 }
 
 public class GetUser : Endpoint<GetUserRequest, MyResponse>
 {
-   public override void Configure()
-   {
-      Get("/api/users/{Id}");
-      AllowAnonymous();
-   }
+	public override void Configure()
+	{
+		Get("/api/users/{Id}");
+		AllowAnonymous();
+	}
 
-   public override async Task HandleAsync(GetUserRequest req, CancellationToken ct)
-   {
-      if (GlobalDataStore.DataStore.TryGetValue(req.Id, out var entity))
-      {
-         var response = MapToResponse(entity, req.Id);
-         await SendOkAsync(response, ct);
-      }
-      else
-      {
-         await SendNotFoundAsync(ct);
-      }
-   }
+	public override async Task HandleAsync(GetUserRequest req, CancellationToken ct)
+	{
+		if (GlobalDataStore.DataStore.TryGetValue(req.Id, out var entity))
+		{
+			var response = MapToResponse(entity, req.Id);
+			await SendOkAsync(response, ct);
+		}
+		else
+		{
+			await SendNotFoundAsync(ct);
+		}
+	}
 
-   private static MyResponse MapToResponse(MyRequest entity, Guid guid) =>
-       new()
-       {
-          Id = guid,
-          FullName = $"{entity.FirstName} {entity.LastName}",
-          IsOver18 = entity.Age >= 18
-       };
+	private static MyResponse MapToResponse(MyRequest entity, Guid guid) =>
+		new()
+		{
+			Id = guid,
+			FullName = $"{entity.FirstName} {entity.LastName}",
+			IsOver18 = entity.Age >= 18
+		};
 }
 
 public class GetUserRequest
 {
-   public Guid Id { get; set; }
+	public Guid Id { get; set; }
 }
 
 public static class GlobalDataStore
 {
-   public static Dictionary<Guid, MyRequest> DataStore = new();
+	public static Dictionary<Guid, MyRequest> DataStore = new();
 }
 
 public class MyRequest
 {
-   public required string FirstName { get; set; }
-   public required string LastName { get; set; }
-   public int Age { get; set; }
+	public required string FirstName { get; set; }
+	public required string LastName { get; set; }
+	public int Age { get; set; }
 }
 
 public class MyResponse
 {
-   public Guid Id { get; set; }
-   public required string FullName { get; set; }
-   public bool IsOver18 { get; set; }
+	public Guid Id { get; set; }
+	public required string FullName { get; set; }
+	public bool IsOver18 { get; set; }
 }
